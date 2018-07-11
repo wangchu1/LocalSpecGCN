5df2e8f65ed47fc8d36a0695f5fd3326796fbf36
Chu Wang <chu.wang@mail.mcgill.ca>
2018-07-11 15:37:58 -0700

    Update pointnet2_cls_ssg.py

--- classification/models/pointnet2_cls_ssg.py
+++ classification/models/pointnet2_cls_ssg.py
@@ -24,7 +24,6 @@ def get_model(point_cloud, is_training, bn_decay=None):
 
     # Set abstraction layers
     l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=32, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1')
-    # changed 128 to 64 according to Qi.
     l2_xyz, l2_points, l2_indices = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=32, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2')
     l3_xyz, l3_points, l3_indices = pointnet_sa_module(l2_xyz, l2_points, npoint=32, radius=0.8, nsample=8, mlp=[256,256,512], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer3')
     l4_xyz, l4_points, l4_indices = pointnet_sa_module(l3_xyz, l3_points, npoint=None, radius=None, nsample=None, mlp=[512,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer4')
